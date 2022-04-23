250e008594c78dbfa8fa607bba122727ce13cf9e
Daiane Silva <daiane.supereira@gmail.com>
2022-04-23 12:37:57 -0300

    layout responsivo okay - sem animação do card

--- src/animacao.js
+++ src/animacao.js
@@ -1,17 +1,21 @@
-window.onload = function () {
-    var card = document.getElementById('card-mobile')
-    card.onclick = mostrarConteudo;
-}
+// window.onload = function () {
+//     var card = document.getElementsByClassName('card-mobile')
+//     console.log(card)
+//     card.onclick = mostrarConteudo;
+// }
 
 
-function mostrarConteudo(){
-    var card_info = document.getElementById('card-mobile-info')
-    if(card_info.style.display === 'block') {
-        console.log("tira conteudo");
-        card_info.style.display = 'none';
-    } else {
-        console.log("coloca conteudo");
-        card_info.style.display = 'block';
-    }
+// function mostrarConteudo(){
+//     var card_info = document.getElementsByClassName('card-mobile-info')
 
-}
+//     if(card_info.style.display === 'block') {
+//         console.log("tira conteudo");
+//         card_info.style.display = 'none';
+//     } else {
+//         console.log("coloca conteudo");
+//         card_info.style.display = 'block';
+//     }
+
+// }
+
+// FAZER COM QUE APENAS A DIV CLICADA APAREÇA AS INFOMRAÇÕES
