e1a70518789b31ee1ec76632c173a5e0c1338d67
Daiane Silva <daiane.supereira@gmail.com>
2022-04-26 08:23:21 -0300

    create login

---
+++ src/config.js
@@ -0,0 +1,22 @@
+
+
+// function requestPokeInfo(url,name){
+//     fetch(url+name)
+//     .then(response=>response.json())
+//     .then(data=>{
+//         pokemon = data;
+//     })
+//     .catch(err => console.log(err))
+//     console.log(pokemon);
+// }
+
+
+// function startApp(pokeName) {
+//     requestPokeInfo(baseUrl, pokeName);
+  
+//     // A função que cria o HTML só será executada 2 segundos depois que a função startApp for executada
+//     setTimeout(function () {
+//         container.innerHTML = createCard();
+//     }, 2000);
+
+// }
